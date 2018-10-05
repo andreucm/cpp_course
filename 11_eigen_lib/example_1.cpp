b8f647ee09228ea1bef86a2f2a1f46ec4b3726a4
andreucm <andreu@beta-robots.com>
2018-10-05 15:23:52 +0200

    Added basic eigen operations

--- 11_eigen_lib/example_1.cpp
+++ 11_eigen_lib/example_1.cpp
@@ -29,11 +29,36 @@ int main(int argc, char *argv[])
 	Eigen::Matrix<double, 3, 3> MI3 = Eigen::Matrix3d::Identity();
 
 	// a 6x6 zero matrix and Identity matrix
-	Eigen::Matrix<double, 6, 6> MZ6 = Eigen::Matrix<double, 6, 6>::Zero(6,6);
-	Eigen::Matrix<double, 6, 6> MI6 = Eigen::Matrix<double, 6, 6>::Identity(6,6);
+	Eigen::Matrix<double, 6, 6> MZ6 = Eigen::Matrix<double, 6, 6>::Zero();
+	Eigen::Matrix<double, 6, 6> MI6 = Eigen::Matrix<double, 6, 6>::Identity();
 
-	// an unknown size matrix
-    Eigen::MatrixXd md;
+	// an unknown size matrix and resizing
+    Eigen::MatrixXd MXd;
+	MXd.resize(4,7);
+	MXd = Eigen::Matrix<double, 4, 7>::Zero();
+	MXd(2,4) = 3.14;
+	MXd(3,6) = -3.14;
+	std::cout << "MXd: " << std::endl << MXd << std::endl;
+
+	// accessing to matrix block
+	std::cout << "MXd(1:3,4:7) = " << MXd.block<3,4>(1,4) << std::endl;
+	std::cout << "MXd(1:3,4:7) = " << MXd.block(1,4,3,4) << std::endl;
+
+	//norm, dot, cross
+	Eigen::Vector3d v1, v2, v3;
+	v1 << 1,  2, 0;
+	v2 << 0, -2, 4.7;
+	v1.normalize();
+	v2.normalize();
+	std::cout << "v1: " << v1.transpose() << std::endl;
+	std::cout << "v2: " << v2.transpose() << std::endl;
+	double dot_result = v1.dot(v2);
+	std::cout << "v1·v2 = " << dot_result << std::endl;  
+
+	//matrix inverse, and check that AA-1 = I
+
+
+
 
     return 0;
 }
