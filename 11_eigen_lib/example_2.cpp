14fbcc825da8995edd84fd50a0f9bb7602a9c9d9
andreucm <andreu@beta-robots.com>
2018-10-08 22:20:38 +0200

    Include cmath instead of math.h. Closes issue #1

--- 11_eigen_lib/example_2.cpp
+++ 11_eigen_lib/example_2.cpp
@@ -9,7 +9,6 @@
 int main(int argc, char *argv[])
 {
 	Eigen::Matrix<double, 3, 1000> P_s;
-
 	P_s.block<2,1000>(0,0) = Eigen::Matrix<double, 2, 1000>::Random();
 	P_s.block<1,1000>(2,0) = Eigen::Matrix<double, 1, 1000>::Ones();
 
